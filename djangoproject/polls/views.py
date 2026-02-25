3b4fa3a528e68018cdf944d97595790bb9f6a93f
Yassine <yassine.zriba@fms-ea.com>
2026-02-25 14:54:43 +0100

    Fix gitignore to track Django app

--- djangoproject/polls/views.py
+++ djangoproject/polls/views.py
@@ -1,13 +1,49 @@
-# polls/views.py
-from django.shortcuts import render
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-def index(request):
-    """
-    Affiche la liste des 5 dernières questions.
-    """
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+from .models import Choice, Question
 
-    # render() = raccourci Django : charge le template + injecte le contexte
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    """
+    Page détail : affiche 1 question + ses choix.
+    get_object_or_404() => renvoie une 404 si l'id n'existe pas.
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    """
+    Page résultats : après le vote, affiche les votes.
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    """
+    Traite le POST du formulaire de vote.
+    """
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Si aucune option choisie => on réaffiche la page détail avec un message d'erreur
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "Tu n'as pas sélectionné de choix.",
+            },
+        )
+
+    # On incrémente les votes
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    # Bonne pratique : Post/Redirect/Get
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
