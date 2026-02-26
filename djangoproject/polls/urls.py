8108c220a7c5d9bf72c4dd51ce497e12e6440904
Yassine <yassine.zriba@fms-ea.com>
2026-02-26 10:57:40 +0100

    feat: add /polls/statistics page with aggregations

--- djangoproject/polls/urls.py
+++ djangoproject/polls/urls.py
@@ -7,6 +7,7 @@ app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
     path("all/", views.AllView.as_view(), name="all"),
+    path("statistics/", views.statistics, name="statistics"),
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
