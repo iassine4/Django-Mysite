1cbecc89367201b45c55629d5b8734a4388845a0
Yassine <yassine.zriba@fms-ea.com>
2026-02-26 12:01:40 +0100

    refactor: reorder polls urls

--- djangoproject/polls/urls.py
+++ djangoproject/polls/urls.py
@@ -5,11 +5,18 @@ from . import views
 app_name = "polls"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    # Routes fixes d'abord
     path("all/", views.AllView.as_view(), name="all"),
     path("statistics/", views.statistics, name="statistics"),
+
+    # Index
+    path("", views.index, name="index"),
+
+    # Routes dynamiques ensuite
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/results/", views.results, name="results"),
+
+    # Actions liées à une question
     path("<int:question_id>/vote/", views.vote, name="vote"),
     path("<int:question_id>/frequency/", views.frequency, name="frequency"),
 ]
